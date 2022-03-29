3ec1cacad026474c3e4d3d3a9813492e27617365
ÃAlberto Ocroche <albrom@gmail.com>
2022-03-29 17:59:08 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -19,3 +19,10 @@ class SubscriptionForm(forms.Form):
     def clean_name(self):
         name = self.cleaned_data['name']
         return name.title().strip()
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+
+        return self.cleaned_data
+
