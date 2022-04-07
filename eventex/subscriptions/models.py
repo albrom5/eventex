b8ffca0619b40f4c2078721d22fe24761244d409
ÃAlberto Ocroche <albrom@gmail.com>
2022-04-07 18:08:18 -0300

    Mixins

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import resolve_url as r
 
 from eventex.subscriptions.validators import validate_cpf
 
@@ -10,6 +11,9 @@ class Subscription(models.Model):
     phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
+
+    def get_absolute_url(self):
+        return r('subscriptions:detail', self.pk)
 
     def __str__(self):
         return self.name
